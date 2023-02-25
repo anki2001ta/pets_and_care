c921d623d4205081882e1d46b2c16cc3ee78072b
Ankita Deb <ankitadeb1202@gmail.com>
2023-02-25 19:58:17 +0530

    added routes in admin section

--- Frontend/src/Pages/Admin/Products.jsx
+++ Frontend/src/Pages/Admin/Products.jsx
@@ -1,11 +1,23 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Center, SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
 import AdminNavbar from './AdminNav'
 const AdminPets = () => {
   return (
     <Box>
        <AdminNavbar>
-        
+        <SimpleGrid columns={2}>
+            <Center>
+            <Center>
+            <Box p={"15px"}>ADD PETS</Box>
+            </Center>
+            <Center>
+            <Box p={"15px"}>DELETE PETS</Box>
+            </Center>
+            <Center>
+            <Box p={"15px"}>MODIFY PETS</Box>
+            </Center>
+            </Center>
+        </SimpleGrid>
        </AdminNavbar>
     </Box>
   )
